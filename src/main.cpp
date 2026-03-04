919ba59d6daf59eb1ce8a6e9c23e90d1e3d7db4b
zeviberlin <zeviberlin@gmail.com>
2026-03-04 12:17:06 -0500

    Templating project

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+// Copyright 2026 The Phi Project Authors
+// Use of this source code is governed by the GPL-3.0 license, found in the LICENSE file
+//
+// Date created: 03-04-2026
+// Original author: Zevi Berlin (zeviberlin@gmail.com)
+// Latest editor: Zevi Berlin (zeviberlin@gmail.com)
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << "Hello, world!\n";
+
+  return 0;
+}
