5fbf7d270fdc0822c1df7e2ebb79bb2ed539fafa
zeviberlin <zeviberlin@gmail.com>
2026-03-05 14:25:03 -0500

    Successful password->key derivation + aegis encryption/decryption cycle

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
-// Copyright 2026 The Phi Project Authors
+// The Phi Project
+// Copyright 2026 Khuzdoor Authors
 // Use of this source code is governed by the GPL-3.0 license, found in the LICENSE file
 //
-// Date created: 03-04-2026
+// Date created: 04-03-2026
 // Original author: Zevi Berlin (zeviberlin@gmail.com)
 // Latest editor: Zevi Berlin (zeviberlin@gmail.com)
 
-#include <iostream>
+//---------> [ Config. Separator ] <---------\\ 
 
 int main(int argc, char** argv) {
-  std::cout << "Hello, world!\n";
-
   return 0;
 }
