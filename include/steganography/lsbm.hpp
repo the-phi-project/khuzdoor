507035d070d1632a937185aea577ea3dc12c85c8
zeviberlin <zeviberlin@gmail.com>
2026-03-21 17:33:49 -0400

    switching LSB -> LSB-M

---
+++ include/steganography/lsbm.hpp
@@ -0,0 +1,44 @@
+// The Phi Project
+// Copyright 2026 Khuzdoor Authors
+// Use of this source code is governed by the GPL-3.0 license, found in the LICENSE file
+//
+// Date created: 09-03-2026
+// Original author: Zevi Berlin (zeviberlin@gmail.com)
+// Latest editor: Zevi Berlin (zeviberlin@gmail.com)
+
+#ifndef LSBM_HPP
+#define LSBM_HPP
+
+#include <string>
+#include <optional>
+#include <span>
+#include <vector>
+#include <bitset>
+#include <cstdint>
+
+#include <sodium.h>
+
+//---------> [ Config. Separator ] <---------\\ 
+
+namespace khuzdoor::steganography {
+
+/// writes a secret bit to a pixel's LSB from a pixel array
+/// via the stbi_load() method. LSBM is when if the pixel's LSB
+/// does not already match the bit to write, it randomly adds or
+/// subtracts 1 (to make pixel match) such that it makes the changed
+/// data look more random
+/// @param pixel pointer to a pixel in the pixel array
+/// @param bit whether the bit should be set to 1 or 0
+void writeLSBM(uint8_t* pixel, bool bit);
+
+//================={ Header Item Separator }=================\\ 
+
+/// reads the secret bit from a pixel's LSB from a pixel array
+/// via the stbi_load() method
+/// @param pixel pointer to a pixel in the pixel array
+/// @returns whether the bit is 1 or 0
+bool readLSBM(const uint8_t* pixel);
+
+}  // namespace khuzdoor::steganography
+
+#endif /* LSBM_HPP */
---
+++ src/lsbm.cpp
@@ -0,0 +1,40 @@
+// The Phi Project
+// Copyright 2026 Khuzdoor Authors
+// Use of this source code is governed by the GPL-3.0 license, found in the LICENSE file
+//
+// Date created: 09-03-2026
+// Original author: Zevi Berlin (zeviberlin@gmail.com)
+// Latest editor: Zevi Berlin (zeviberlin@gmail.com)
+
+#include "steganography/lsbm.hpp"
+
+#include <cstddef>
+#include <string>
+#include <optional>
+#include <span>
+#include <vector>
+#include <bitset>
+#include <cstdint>
+
+#include <sodium.h>
+
+//---------> [ Config. Separator ] <---------\\ 
+
+void khuzdoor::steganography::writeLSBM(uint8_t* pixel, bool bit) {
+  if ((*pixel & 1U) == (uint8_t)bit) {  // pixel's LSB already is bit
+    return;                             // no change necessary
+  }
+
+  // basically just a pseudo-random way to determine +/-
+  if (randombytes_random() % 2 == 0) {
+    *pixel = (*pixel == 255) ? 254 : *pixel - 1;  // avoid overflow
+  } else {
+    *pixel = (*pixel == 0) ? 1 : *pixel + 1;  // avoid underflow
+  }
+}
+
+//------------[ Func. Implementation Separator ]------------\\ 
+
+bool khuzdoor::steganography::readLSBM(const uint8_t* pixel) {
+  return (*pixel & 1U) != 0;
+}
