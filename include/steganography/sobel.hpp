f3971902a7de0c579620a59d9ea6d709f5492009
zeviberlin <zeviberlin@gmail.com>
2026-04-05 12:31:50 -0600

    successful RGB -> Gray Scale function for pixels

---
+++ include/steganography/sobel.hpp
@@ -0,0 +1,41 @@
+// The Phi Project
+// Copyright 2026 Khuzdoor Authors
+// Use of this source code is governed by the GPL-3.0 license, found in the LICENSE file
+//
+// Date created: 05-04-2026
+// Original author: Zevi Berlin (zeviberlin@gmail.com)
+// Latest editor: Zevi Berlin (zeviberlin@gmail.com)
+
+#ifndef SOBEL_HPP
+#define SOBEL_HPP
+
+/************** SOBEL'S OPERATOR EXPLAINED **************
+
+********************************************************/
+
+#include <array>
+#include <cstdint>
+#include <cmath>  // ::abs(), std::round()
+
+#define SOBEL_MATRIX_SIZE_X 4  // edge matrix will be
+#define SOBEL_MATRIX_SIZE_Y 3  // 3x3, as seen above
+
+//---------> [ Config. Separator ] <---------\\ 
+
+namespace khuzdoor::steg {
+
+/// Convert an RGB pixel to a grayscale one
+/// @param red the 0..255 value for red
+/// @param green the 0..255 value for red
+/// @param blue the 0..255 value for red
+/// @returns the 0..255 value for grayscale intensity
+/// @note relies on the linear tranformation defined in
+///       the SO answer https://stackoverflow.com/a/17619494
+inline uint8_t RGB2GS(uint8_t red, uint8_t green, uint8_t blue) {
+  double lum = (0.2126 * red) + (0.7152 * green) + (0.0722 * blue);
+  return static_cast<uint8_t>(std::round(lum));
+}
+
+}  // namespace khuzdoor::steg
+
+#endif /* SOBEL_HPP */
